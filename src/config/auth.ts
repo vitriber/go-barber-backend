393eded003fd3ae0a7ce2c8a22ed2f92c4ee69d5
Vitor <vit.riber1@hotmail.com>
2020-10-30 15:49:04 -0300

    remove gitignore files

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     jwt: {
-        secret: 'e49767c097e930a54a98ad8c1684606a',
+        secret: process.env.APP_SECRET,
         expiresIn: '1d',
     },
 };
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticate';
 import ProvidersController from '../controllers/ProvidersController';
@@ -16,10 +17,20 @@ providersRouter.use(ensureAuthenticaded);
 providersRouter.get('/', providersController.index);
 providersRouter.get(
     '/:provider_id/month-availability',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
     providerMonthAvailabilityController.index,
 );
 providersRouter.get(
     '/:provider_id/day-availability',
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }),
     providerDayAvailabilityController.index,
 );
 
