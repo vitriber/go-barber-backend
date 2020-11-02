b5810db69cfb274c903bd4aa0ce5d6958d9c9592
Vitor <vit.riber1@hotmail.com>
2020-11-02 12:45:03 -0300

    organize files and update mail

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+    driver: 'ethereal' | 'ses';
+
+    defaults: {
+        from: {
+            email: string;
+            name: string;
+        };
+    };
+}
+
+export default {
+    driver: process.env.MAIL_DRIVER || 'ethereal',
+
+    defaults: {
+        from: {
+            email: 'vitor@vrscode.com.br',
+            name: 'Vitor da VRSCODE',
+        },
+    },
+} as IMailConfig;
